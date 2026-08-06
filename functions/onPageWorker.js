1cccbddb6e5e427f5459ddf1052fe6a03715c082
sixten <sixho495@student.liu.se>
2026-08-06 20:50:47 +0200

    try catch på workern

--- functions/onPageWorker.js
+++ functions/onPageWorker.js
@@ -1,32 +1,42 @@
 export async function onRequest(context) {
-    const database = context.env.dattabas;
+    try {
 
-    if (context.request.method === "POST") {
-        const data = await context.request.json();
-        var iCal = data.iCal;
-        var id = data.id;
-
-        await database
-            .prepare("INSERT OR REPLACE INTO calendar (id, iCal) VALUES (?, ?)")
-            .bind(id,iCal)
-            .run();
-
-        return new Response("Kalender sparad");
-    }
-
-    if (context.request.method === "GET") {
-        const calendar = await database
-            .prepare("SELECT ics FROM calendar WHERE id = 1")
-            .first();
-
-        if (!calendar) {
-            return new Response("Ingen kalender finns");
+        const database = context.env.dattabas;
+        
+        if (context.request.method === "POST") {
+            const data = await context.request.json();
+            var iCal = data.iCal;
+            var id = data.id;
+        
+            await database
+                .prepare("INSERT OR REPLACE INTO calendar (id, iCal) VALUES (?, ?)")
+                .bind(id,iCal)
+                .run();
+        
+            return new Response("Kalender sparad");
+        }
+    
+        if (context.request.method === "GET") {
+            const calendar = await database
+                .prepare("SELECT ics FROM calendar WHERE id = 1")
+                .first();
+        
+            if (!calendar) {
+                return new Response("Ingen kalender finns");
+            }
+        
+            return new Response(calendar.ics, {
+                headers: {
+                    "Content-Type": "text/calendar"
+                }
+            });
         }
 
-        return new Response(calendar.ics, {
-            headers: {
-                "Content-Type": "text/calendar"
-            }
+    } catch (error) {
+        console.log(error);
+
+        return new Response(error.stack, {
+            status: 500
         });
     }
 }
