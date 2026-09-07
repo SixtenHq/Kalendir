35fb5926cadb0f289e389cf42a1d4005e95efe68
sixten <sixho495@student.liu.se>
2026-09-07 08:42:50 +0200

    glömde paranteser

--- functions/import.js
+++ functions/import.js
@@ -19,9 +19,9 @@ export async function onRequest(context) {
     }
     const data = JSON.parse(result.data);
     dt.importData(data);
-    console.log(dt.DataToString);
+    console.log(dt.DataToString());
     updateCal();
-    console.log(dt.DataToString);
+    console.log(dt.DataToString());
     const ics = dt.getIcs();
 
     return new Response(ics, {
