f56e54103a62b1c760b7dbf348cd519329c84792
sixten Holmqvist <sixho495@student.liu.se>
2026-09-15 17:33:24 +0200

    parcer done for now

--- utils/calendar2.js
+++ utils/calendar2.js
@@ -1,66 +1,44 @@
 import ICAL from "ical.js";
 import * as dt from "./data.js";
 import * as time from "./time.js";
+import { parceIcs } from "./icalParcer.js";
 
 
 export async function updateCal() {
+    time.start();
     await importCal();
+    time.end();
 }
 
 async function importCal() {
-        
+    
     const CalSorceLinks = dt.getCalSorces();
     const savedEvents = dt.getEvents();
     const newEventList = new Map();
     
     //import new events
     for (const link of CalSorceLinks) {
-        
+        time.pause();
         const response = await fetch(link);
         const ics = await response.text();
-        const comps = new ICAL.Component(ICAL.parse(ics));
-        const importedEvents = comps.getAllSubcomponents("vevent");
+        time.unpause();
+        let importedEvents = parceIcs(ics);
         const currentTime = Date.now();
-        
-        time.start();
-        time.pause();
-        for (const e of importedEvents) {
-            
-            time.unpause();
-            const id = e.getFirstPropertyValue("uid");
-            time.pause();
+
+        for (const impEvent of importedEvents) {            
+            const id = impEvent.id;
             const savedEvent = savedEvents.get(id);
             
-            const importedEventLastModified = e.getFirstPropertyValue("last-modified").toJSDate();
-            
-            if (!savedEvent || importedEventLastModified < savedEvent.lastModified) {
-                newEventList.set(id, {
-                    start: e.getFirstPropertyValue("dtstart").toJSDate(),
-                    end: e.getFirstPropertyValue("dtend").toJSDate(),
-                    id: id,
-                    stamp: e.getFirstPropertyValue("dtstamp").toJSDate(),
-                    lastModified: importedEventLastModified,
-                    url: e.getFirstPropertyValue("url"),
-                    summary: e.getFirstPropertyValue("summary"),
-                    location: e.getFirstPropertyValue("location"),
-                    description: e.getFirstPropertyValue("description")
-                });
+            if (!savedEvent || impEvent.lastModified < savedEvent.lastModified) {
+                newEventList.set(id, impEvent);
                 savedEvents.delete(id);
-            } else {
-                newEventList.set(id, savedEvent); // FEL!!!!!!!!!!!!!!!!!!!!
-            }
-            
+            } 
         } 
     }    
-    time.end();
     // spara gamla event
-    for (const e of savedEvents) {
-        if (!newEventList.get(e.id) && e.start < currentTime) {
-            newEventList.set(e.id, e);
+    for (const event of savedEvents) {
+        if (!newEventList.get(event.id) && event.start < currentTime) {
+            newEventList.set(event.id, eevnet);
         }
     }
-}
-
-function makeCal() {
-
 }
---
+++ utils/icalParcer.js
@@ -0,0 +1,92 @@
+export function parceIcs(ics = "") {
+    const events = [];
+    let pos = 0;
+    let nextPos;
+    while (ics.indexOf("BEGIN", pos) != -1) {
+        const event = {}
+
+        pos = ics.indexOf("DTSTART:", pos);
+
+        nextPos = ics.indexOf("DTEND:", pos);
+        event["start"] = toJSDate(ics.slice(pos + 8, nextPos - 2));
+        pos = nextPos;
+
+        nextPos = ics.indexOf("UID:", pos);
+        event["end"] = toJSDate(ics.slice(pos + 6, nextPos - 2));
+        pos = nextPos;
+
+        nextPos = ics.indexOf("DTSTAMP:", pos);
+        event["id"] = ics.slice(pos + 4, nextPos - 2);
+        pos = nextPos;
+
+        nextPos = ics.indexOf("LAST-MODIFIED:", pos);
+        event["stamp"] = ics.slice(pos + 8, nextPos - 2);
+        pos = nextPos;
+
+        nextPos = ics.indexOf("URL:", pos);
+        event["lastModified"] = toJSDate(ics.slice(pos + 14, nextPos - 2));
+        pos = nextPos;
+
+        nextPos = ics.indexOf("SUMMARY:", pos);
+        event["url"] = unescapeICS(ics, pos + 4, nextPos - 2);
+        pos = nextPos;
+
+        nextPos = ics.indexOf("LOCATION:", pos);
+        event["summary"] = unescapeICS(ics, pos + 8, nextPos - 2);
+        pos = nextPos;
+
+        nextPos = ics.indexOf("DESCRIPTION:", pos);
+        event["location"] = unescapeICS(ics, pos + 9, nextPos - 2);
+        pos = nextPos;
+
+        nextPos = ics.indexOf("END:", pos);
+        event["description"] = unescapeICS(ics, pos + 12, nextPos - 2);
+        pos = nextPos;
+
+        events.push(event);
+    }
+    
+    return events;
+    
+}
+
+
+function unescapeICS(ics,startPos,endPos) {
+    let cuts = [startPos -1];
+
+    for (let pos = startPos; pos < endPos; pos++) {
+        if (ics[pos] == "\\" && pos + 1 < endPos) {
+            const nextChar = ics[++pos];
+
+            if (nextChar == ",") cuts.push(pos - 1);
+            else if (nextChar == ";") cuts.push(pos - 1);
+        } else if (ics[pos] == "\r" && ics[pos + 1] == "\n") {
+            cuts.push(pos - 1);
+            cuts.push(pos);
+            cuts.push(pos + 1);
+            cuts.push(pos + 2);
+        }
+    }
+    cuts.push(endPos);
+
+    let result = "";
+    for (let i = 0; i < cuts.length -1; i++) {
+        result += ics.slice(cuts[i] + 1, cuts[i + 1]);
+    }
+    result = result.replaceAll("\\n","\n");
+    
+    return result;
+}
+
+function toJSDate(icsTime) {
+    return new Date(
+        Date.UTC(
+            Number(icsTime.slice(0, 4)),
+            Number(icsTime.slice(4, 6)) - 1,
+            Number(icsTime.slice(6, 8)),
+            Number(icsTime.slice(9, 11)),
+            Number(icsTime.slice(11, 13)),
+            Number(icsTime.slice(13, 15))
+        )
+    );
+}
